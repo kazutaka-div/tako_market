a6a21b62fa8e31ff53e5a2b769fc3e16632e195f
kazukimaeda26 <kazukimaeda26@gmail.com>
2020-10-12 22:45:03 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   #以下の記述でrouthingが変更となる。users/registrations#newなど。
   #devise_for :users であると、devise/registrations#newなど。
-    # devise_for :users, controllers: {
-    #   registrations: 'users/registraions'
-    # }
 end
