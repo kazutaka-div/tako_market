0a2db4a6c0aa0abeee08d93f66d730e32a2d2537
kazukimaeda26 <kazukimaeda26@gmail.com>
2020-10-12 22:18:26 +0900

    deviseのルーティング名の変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   devise_for :users
+
+  #以下の記述でrouthingが変更となる。users/registrations#newなど。
+  #devise_for :users であると、devise/registrations#newなど。
+    # devise_for :users, controllers: {
+    #   registrations: 'users/registraions'
+    # }
 end
