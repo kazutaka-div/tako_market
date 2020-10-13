53a3790d1486bcfcff6111631c850380607b985d
kazukimaeda26 <kazukimaeda26@gmail.com>
2020-10-13 17:56:45 +0900

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-
   #以下の記述でrouthingが変更となる。users/registrations#newなど。
   #devise_for :users であると、devise/registrations#newなど。
+  devise_scope :user do
+    get 'address', to: 'users/registrations#new_adrress'
+    post 'address', to: 'users/registrations#create_address'
+  end
 end
