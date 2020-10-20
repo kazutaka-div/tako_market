07ee6e4eedfa82e39249780743d5a49102fef698
kazukimaeda26 <kazukimaeda26@gmail.com>
2020-10-20 20:31:03 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
   
   def index
     @items = Item.last(4)
   end
 
   def new
-    
     @item = Item.new
     3.times do
       @item.images.new
@@ -21,8 +21,26 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path, notice:"編集に成功したよ"
+    else
+      render :edit
+    end
+  end
+
+
   private
   def item_params
     params.require(:item).permit(:name, :price, :status,:prefecture_id, images_attributes: [:src])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
